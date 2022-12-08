1fd70f87da56923b34c599319741bbc0416558e4
ivandancil <106507975+ivandancil@users.noreply.github.com>
2022-12-09 00:46:59 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,225 @@
+let allMusic = [
+    {
+        name: "Because of You",
+        artist: "Ne-Yo",
+        img: "image_1",
+        src: "music_1"
+    },
+    {
+        name: "Wet the Bed",
+        artist: "Chris Brown",
+        img: "image_2",
+        src: "music_2"
+    },
+    {
+        name: "Swalla",
+        artist: "Jason Derulo",
+        img: "image_3",
+        src: "music_3"
+    },
+    {
+        name: "Mad",
+        artist: "Ne-Yo",
+        img: "image_4",
+        src: "music_4"
+    },
+    {
+        name: "Miss Independent",
+        artist: "Ne-Yo",
+        img: "image_5",
+        src: "music_5"
+    },
+    {
+        name: "Booty Wurk",
+        artist: "T-pain",
+        img: "image_6",
+        src: "music_6"
+    },
+    {
+        name: "Together",
+        artist: "Ne-Yo",
+        img: "image_1",
+        src: "music_7"
+    },
+    {
+        name: "Next to Your",
+        artist: "Chris Brown",
+        img: "image_2",
+        src: "music_8"
+    },
+    {
+        name: "Beautiful Girls",
+        artist: "Sean Kingston",
+        img: "image_3",
+        src: "music_9"
+    },
+    {
+        name: "Payphone",
+        artist: "Maroon 5 ft. Wiz Khalifa",
+        img: "image_4",
+        src: "music_10"
+    },
+    {
+        name: "Fire Burning",
+        artist: "Sean Kingston",
+        img: "image_5",
+        src: "music_11"
+    },
+    {
+        name: "Me Lovee",
+        artist: "Sean Kingston",
+        img: "image_6",
+        src: "music_12"
+    },
+    {
+        name: "So Sick",
+        artist: "Ne-Yo",
+        img: "image_4",
+        src: "music_13"
+    },
+    {
+        name: "Titig",
+        artist: "Audio",
+        img: "image_10",
+        src: "music_15"
+    },
+    {
+        name: "Bahala Ka",
+        artist: "Audio",
+        img: "image_8",
+        src: "music_16"
+    },
+    {
+        name: "Wag na lang",
+        artist: "Audio",
+        img: "image_12",
+        src: "music_17"
+    },
+    {
+        name: "24 hours",
+        artist: "Cueshe",
+        img: "image_1",
+        src: "music_18"
+    },
+    {
+        name: "Habang Buhay",
+        artist: "Zack Tabudlo",
+        img: "image_2",
+        src: "music_19"
+    },
+    {
+        name: "When I met you",
+        artist: "Reyne(cover)",
+        img: "image_3",
+        src: "music_20"
+    },
+    {
+        name: "Weak",
+        artist: "Khel Pangilinan(cover)",
+        img: "image_4",
+        src: "music_21"
+    },
+    {
+        name: "Very Special Someone",
+        artist: "Reyne(Cover)",
+        img: "image_5",
+        src: "music_22"
+    },
+    {
+        name: "Umaaraw_umuulan",
+        artist: "Rivermaya",
+        img: "image_6",
+        src: "music_23"
+    },
+    {
+        name: "Think I'm in Love Again",
+        artist: "Reyne(Cover)",
+        img: "image_7",
+        src: "music_24"
+    },
+    {
+        name: "The Only One",
+        artist: "Reyne(Cover)",
+        img: "image_8",
+        src: "music_25"
+    },
+    {
+        name: "Tahanan",
+        artist: "Adie",
+        img: "image_9",
+        src: "music_26"
+    },
+    {
+        name: "Sheena Alaala",
+        artist: "unknown",
+        img: "image_10",
+        src: "music_27"
+    },
+    {
+        name: "Love can Hurt",
+        artist: "Range",
+        img: "image_11",
+        src: "music_28"
+    },
+    {
+        name: "Oks Nako",
+        artist: "Range",
+        img: "image_12",
+        src: "music_29"
+    },
+    {
+        name: "No One Else Comes Close",
+        artist: "Reyne(Cover)",
+        img: "image_1",
+        src: "music_30"
+    },
+    {
+        name: "Muli",
+        artist: "Reyne(Cover)",
+        img: "image_2",
+        src: "music_31"
+    },
+    {
+        name: "Martyr Nyebera",
+        artist: "Kamikazee",
+        img: "image_3",
+        src: "music_32"
+    },
+    {
+        name: "Mandy",
+        artist: "Reyne(Cover)",
+        img: "image_4",
+        src: "music_33"
+    },
+    {
+        name: "Just Once",
+        artist: "Reyne(Cover)",
+        img: "image_5",
+        src: "music_34"
+    },
+    {
+        name: "If Ever your In my Arms Again",
+        artist: "Reyne(Cover)",
+        img: "image_6",
+        src: "music_35"
+    },
+    {
+        name: "I Just Called to Say I Love You",
+        artist: "Reyne(Cover)",
+        img: "image_7",
+        src: "music_36"
+    },
+    {
+        name: "Chiksilog",
+        artist: "KamiKazee",
+        img: "image_8",
+        src: "music_37"
+    },
+    {
+        name: "Against All Odds",
+        artist: "Francis Greg",
+        img: "image_9",
+        src: "music_38"
+    },
+ 
+]
